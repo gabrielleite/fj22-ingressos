1e71d4e42fd69a5dba5942c2e41c24235e20e5af
Gabriel Leite <gabrielcfcleite@gmail.com>
2017-09-14 12:23:29 -0300

    Solução do exercício 3.2.

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 
@@ -22,6 +23,8 @@ public class Sessao {
 	@ManyToOne
 	private Filme filme;
 
+	private BigDecimal preco;
+
 	/*** @deprecated hibernate only */
 	public Sessao() {
 	}
@@ -30,6 +33,7 @@ public class Sessao {
 		this.horario = horario;
 		this.setFilme(filme);
 		this.sala = sala;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 
 	public Sala getSala() {
@@ -62,6 +66,14 @@ public class Sessao {
 
 	public void setFilme(Filme filme) {
 		this.filme = filme;
+	}
+	
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
 	}
 
 	public LocalTime getHorarioTermino() {
--- src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.validacao;
 
+import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
@@ -16,11 +18,11 @@ import br.com.caelum.ingresso.model.Sessao;
 public class GerenciadorDeSessaoTest {
 	@Test
 	public void garanteQueNaoDevePermitirSessaoNoMesmoHorario() {
-		Filme filme = new Filme();
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120),  "SCI-FI", BigDecimal.ONE);
 		filme.setDuracao(120);
 
 		LocalTime horario = LocalTime.now();
-		Sala sala = new Sala("");
+		Sala sala = new Sala("", BigDecimal.ONE);
 
 		Sessao sessao = new Sessao(horario, filme, sala);
 
@@ -32,11 +34,11 @@ public class GerenciadorDeSessaoTest {
 
 	@Test
 	public void garanteQueNaoDevePermitirSessoesTerminandoDentroDoHorarioDeUmaSessaoJaExistente() {
-		Filme filme = new Filme();
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120),  "SCI-FI", BigDecimal.ONE);
 		filme.setDuracao(120);
 
 		LocalTime horario = LocalTime.now();
-		Sala sala = new Sala("");
+		Sala sala = new Sala("Eldorado - IMAX", BigDecimal.ONE);
 
 		List<Sessao> sessoes = Arrays.asList(new Sessao(horario.plusHours(1), filme, sala));
 		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
@@ -48,11 +50,11 @@ public class GerenciadorDeSessaoTest {
 
 	@Test
 	public void garanteQueNaoDevePermitirSessoesIniciandoDentroDoHorarioDeUmaSessaoJaExistente() {
-		Filme filme = new Filme();
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120),  "SCI-FI", BigDecimal.ONE);
 		filme.setDuracao(120);
 
 		LocalTime horario = LocalTime.now();
-		Sala sala = new Sala("");
+		Sala sala = new Sala("Eldorado - IMAX", BigDecimal.ONE);
 
 		List<Sessao> sessoesDaSala = Arrays.asList(new Sessao(horario, filme, sala));
 		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoesDaSala);
@@ -64,15 +66,15 @@ public class GerenciadorDeSessaoTest {
 
 	@Test
 	public void garanteQueDevePermitirUmaInsercaoEntreDoisFilmes() {
-		Sala sala = new Sala("");
+		Sala sala = new Sala("Eldorado - IMAX", BigDecimal.ONE);
 		
-		Filme filme1 = new Filme();
+		Filme filme1 = new Filme("Rogue One", Duration.ofMinutes(120),  "SCI-FI", BigDecimal.ONE);
 		filme1.setDuracao(90);
 		
 		LocalTime dezHoras = LocalTime.parse("10:00:00");
 		Sessao sessaoDasDez = new Sessao(dezHoras, filme1, sala);
 		
-		Filme filme2 = new Filme();
+		Filme filme2 = new Filme("Rogue One", Duration.ofMinutes(120),  "SCI-FI", BigDecimal.ONE);
 		filme2.setDuracao(120);
 		
 		LocalTime dezoitoHoras = LocalTime.parse("18:00:00");
