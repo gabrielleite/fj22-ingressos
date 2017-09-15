aada96966987fb51af03f4967d34665ff5cbec20
Gabriel Leite <gabrielcfcleite@gmail.com>
2017-09-14 21:58:42 -0300

    Solução do exercício 6.4.

--- src/main/java/br/com/caelum/ingresso/model/Carrinho.java
+++ src/main/java/br/com/caelum/ingresso/model/Carrinho.java
@@ -14,8 +14,13 @@ public class Carrinho {
 	public void add(Ingresso ingresso) {
 		ingressos.add(ingresso);
 	}
-	
+
 	public List<Ingresso> getIngressos() {
 		return ingressos;
+	}
+
+	public boolean isSelecionado(Lugar lugar) {
+		return ingressos.stream().map(Ingresso::getLugar)
+				.anyMatch(l -> l.equals(lugar));
 	}
 }
